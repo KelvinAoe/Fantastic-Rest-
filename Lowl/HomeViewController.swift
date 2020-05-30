466b33e23e9566df54e4318c90b5cfd863ab605d
Kelvin Aoe <kelvinaoe10@gmail.com>
2020-05-30 23:10:49 +0700

    Update 2

--- Lowl/HomeViewController.swift
+++ Lowl/HomeViewController.swift
@@ -5,10 +5,8 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
-                          self.performSegue(withIdentifier: "FromID", sender: self)
-                  
-                    })
+        
+      
     }
     
 
--- Lowl/JumpView.swift
+++ Lowl/JumpView.swift
@@ -11,7 +11,10 @@ class JumpView: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-         
+         DispatchQueue.main.asyncAfter(deadline: .now() + 0.1, execute: {
+            self.performSegue(withIdentifier: "FromID", sender: self)
+            
+    })
             
     }
     
